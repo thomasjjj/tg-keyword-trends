c591089387f27723ed82743ed7b9f38ae47fc60a
tomj <118008765+thomasjjj@users.noreply.github.com>
2023-04-18 02:27:01 +0100

    multiple fixes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tg-keyword-trends',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='tomja',
+    author_email='',
+    description=''
+)
